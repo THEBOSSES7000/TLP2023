31ada6a27fc4373b55b6dd7af47528b929b598b4
ElJoaZ <68742555+THEBOSSES7000@users.noreply.github.com>
2023-05-03 13:51:22 -0300

    Add files via upload

---
+++ JoaYFran23.py
@@ -0,0 +1,9 @@
+x=1
+suma=0
+suma2=0
+emp=int(input("ingrese numero de empleados "))
+while x<=emp:
+    num1=int(input("Sueldo: "))
+    suma=suma+num1
+    x=x+1
+print("El sueldo sumado es:",num1)
